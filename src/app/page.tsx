9c7157d2f2f996bdf38d3b03152cf8bafc25a431
Weiyi Xu <johnsonxwy@gmail.com>
2025-12-21 18:47:58 +0800

    feat: add homepage boilerplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,40 @@
+import { ArrowUpRight } from 'lucide-react'
+
 export default function Page() {
-  return <div>hi</div>
+  return (
+    <div className='min-h-screen flex flex-col items-center justify-center'>
+      <div className='flex flex-col font-medium max-w-xl gap-y-2 md:gap-y-4 px-6'>
+        <p>Hi, I&apos;m Will 👋</p>
+        <p>
+          I&apos;m a frontend developer at{' '}
+          <a
+            href='https://www.mi.com/global/'
+            target='_blank'
+            className='inline-flex items-center underline'
+          >
+            Xiaomi <ArrowUpRight size={16} />
+          </a>
+        </p>
+        <p>This site is still a work in progress</p>
+        <p>
+          You can find my code on{' '}
+          <a
+            href='https://github.com/Johnson1602'
+            target='_blank'
+            className='inline-flex items-center underline'
+          >
+            GitHub <ArrowUpRight size={16} />
+          </a>{' '}
+          and my thoughts on{' '}
+          <a
+            href='https://x.com/willxuu_'
+            target='_blank'
+            className='inline-flex items-center underline'
+          >
+            X <ArrowUpRight size={16} />
+          </a>
+        </p>
+      </div>
+    </div>
+  )
 }
